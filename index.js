34ee15761cd34437fc69698c5db4ef78b40beb85
HomeMac <homemac@HomeMacs-MacBook-Pro.local>
2022-02-05 10:50:12 -0800

    painting updated

---
+++ index.js
@@ -0,0 +1,44 @@
+let selectedColor = 'white'
+
+const painting = document.querySelector('.painting')
+
+let clearButton = document.createElement('button')
+clearButton.textContent = 'Clear'
+document.body.appendChild(clearButton)
+
+clearButton.addEventListener('click', function(){
+   painting.style.backgroundColor ='white'
+})
+
+painting.addEventListener('click', function(e){
+    e.target.style.backgroundColor = selectedColor 
+})
+//Double click pixel
+painting.addEventListener('dblclick', function(e){
+    selectedColor = 'white'
+    //e.target.style.backgroundColor = 'white' 
+})
+// Create Blue color pallet object
+let colorBlue = document.getElementById('blue')
+//Add click event to Blue pallet
+colorBlue.addEventListener('click', function(){
+    selectedColor = 'blue'
+})
+
+let colorRed = document.getElementById('red')
+colorRed.addEventListener('click', function (){
+    selectedColor = 'red'
+})
+
+let colorYellow = document.getElementById ('yellow')
+colorYellow.addEventListener('click', function(){
+    selectedColor = 'yellow'
+})
+let colorGreen = document.getElementById('green')
+colorGreen.addEventListener('click', function(){
+    selectedColor = 'green'
+})
+let colorPurple = document.getElementById('purple')
+colorPurple.addEventListener('click', function(){
+    selectedColor = 'purple'
+})
